ea8300c772006659056488efed3f06b7f1c6e3d2
clio001 <johnwoitkowitz@hotmail.com>
2025-03-29 23:26:38 +0100

    add db openings

--- app.py
+++ app.py
@@ -9,5 +9,22 @@ def index():
     with open('static/db/docs.json', 'r', encoding='utf-8') as f:
         docs = json.load(f)
         f.close()
-    return render_template('index.html', docs=docs)
+    
+    with open('static/db/articles.json', 'r', encoding='utf-8') as f:
+        articles = json.load(f)
+        f.close()
 
+    with open('static/db/blogs.json', 'r', encoding='utf-8') as f:
+        blogs = json.load(f)
+        f.close()
+
+    with open('static/db/backyard.json', 'r', encoding='utf-8') as f:
+        backyard = json.load(f)
+        f.close()
+        
+    return render_template('index.html', docs=docs, articles=articles, blogs=blogs, backyard=backyard)
+
+
+@app.route('/item')
+def item():
+    return render_template('item.html')
